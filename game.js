ddb4508cb0e3d9dc29d66d0f24836c596ccdcc3c
Weston Dockstader <dockstaderweston@gmail.com>
2018-04-27 13:00:19 -0600

    added Target and Options constructors

--- game.js
+++ game.js
@@ -1,30 +1,53 @@
-var health=100;
-var name="The Guy";
-var hits=0;
-document.getElementById('name').innerText=`${name}`;
+// var health=100;
+// var name="The Guy";
+// var hits=0;
+//document.getElementById('name').innerText=`${name}`;
+// this code was used before I created my constructor Target
+
+// This constructor is for creating opponents or targets
+function Target(health,name){
+    this.health=health;
+    this.name=name;
+    this.hits=0;
+}
+var target=new Target(120,"The Guy");
+
+function Options(name,modifier,description){
+    this.attackName= name;
+    this.modifier=modifier;
+    this.description=description; 
+}
 
 function slap(){
-    health--;
-    hits++;
+    target.health--;
+    target.hits++;
     update();
 }
 
 function punch(){
-    health-=5;
-    hits++;
+    target.health-=5;
+    target.hits++;
     update();
 }
 
 function kick(){
-    health-=10;
-    hits++;
+    target.health-=10;
+    target.hits++;
     update();
 }
 
 // This function is for updating the user interface whenever a value changes.
 function update(){
-   document.getElementById('health').innerText= `${health}`;
-   document.getElementById('hits').innerText= `${hits}`; 
+    if(target.health<0){
+        target.health=0;
+    }
+   document.getElementById('health').innerText= `${target.health}`;
+   document.getElementById('hits').innerText= `${target.hits}`; 
 }
 
-update();
+function init(){
+    update();
+    document.getElementById('name').innerText=`${target.name}`;
+}
+
+init();
