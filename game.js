452b680825a2472abb8a2881365e40b84cd7a6be
Weston Dockstader <dockstaderweston@gmail.com>
2018-04-27 11:01:37 -0600

    Step 6 - HTML added punch and kick and their functions

--- game.js
+++ game.js
@@ -1,13 +1,29 @@
 var health=100;
+var name="The Guy";
+var hits=0;
 
 function slap(){
     health--;
+    hits++;
+    update();
+}
+
+function punch(){
+    health-=5;
+    hits++;
+    update();
+}
+
+function kick(){
+    health-=10;
+    hits++;
     update();
 }
 
 // This function is for updating the user interface whenever a value changes.
 function update(){
-   document.getElementById('health').innerText= `${health}`; 
+   document.getElementById('health').innerText= `${health}`;
+   document.getElementById('hits').innerText= `${hits}`; 
 }
 
 update();
